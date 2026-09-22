aea4d68273bc7e88bb485c8ee1d36fe6a1b5e203
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2026-09-22 21:51:17 +0530

    Revert "feat: implement server-side rendering with Angular and update configurations"
    This reverts commit 50712008bf1bd5d9741aca460fb303f2dedde691.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withHashLocation, withComponentInputBinding } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withHashLocation(), withComponentInputBinding()),
-    provideClientHydration(withEventReplay())
+    provideRouter(routes, withHashLocation(), withComponentInputBinding())
   ]
 };
