50712008bf1bd5d9741aca460fb303f2dedde691
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2026-09-22 11:16:01 +0530

    feat: implement server-side rendering with Angular and update configurations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withHashLocation, withComponentInputBinding } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withHashLocation(), withComponentInputBinding())
+    provideRouter(routes, withHashLocation(), withComponentInputBinding()),
+    provideClientHydration(withEventReplay())
   ]
 };
